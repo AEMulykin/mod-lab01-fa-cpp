647dfe9411341aeacd7aba6176c2c0d90168bd26
AEMulykin <124257311+AEMulykin@users.noreply.github.com>
2024-03-31 12:18:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char* str = "Arvysd odso!";
+    std::cout << "faStr1: " << faStr1(str) << std::endl;
+    std::cout << "faStr2: " << faStr2(str) << std::endl;
+    std::cout << "faStr3: " << faStr3(str) << std::endl;
     return 0;
 }
